27528afc0f9fc7ea9fd7be4738f8fda981aec240
Eli <eli.parkhurst@gmail.com>
2022-12-23 11:02:10 -0700

    added comments

--- solution.js
+++ solution.js
@@ -1,5 +1,19 @@
 import map from './data.mjs'
+// This solution was my first run through. It is not very efficent
+// but shows the steps I went thgouh to get an answer
+// 1. loop through everything eg search()
+// 2. check the postions around at each postion
+// 3. convert Xs to numbers for checking
+// 4. didn't work so add consolidation function
 
+
+
+// the search function pretty much goes through each position
+// if an element is an x it checks it's neighbors to see if any are a number
+// if yes, it becomes that number, if no, it gets the next 'lastIslan' number
+// this mostly worked but created situations where an x that was part of a large island
+// might get a new number whe a previously found position was more than one away
+// the consolidate function then cleans it up
 const search = () => {
   let lastIsland = 0
   for (let y = 0; y < map.length; y++) {
@@ -21,6 +35,8 @@ const search = () => {
   // console.log(map.map((row) => console.log(row.join(''))))
 }
 
+// checkSurroundings is given a postion and checks all the places around
+// than postion on the map for a number
 const checkSurroundings = (y, x, map) => {
   if (y === 1 && x === 4) {
     console.log(map[y]?.[x + 1])
@@ -41,6 +57,10 @@ const checkSurroundings = (y, x, map) => {
   return false
 }
 
+// go through a map that has land converted to numbers
+// it then makes the numbers match
+// and island of 0032220 becomes 0022220
+// it loops on more time to create a dictionary of the numbers and get a final count
 const consolidate = (map) => {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
--- solution2.js
+++ solution2.js
@@ -1,5 +1,12 @@
 import data from './data.mjs'
 
+// This is my second pas at the problem and I like it a lot more
+// It follows the same principal of converting Xs to numbers but does
+// it in a more efficent way.
+// Once an X is found it recursively converts the whole island to that number
+// Because there aren't mistakes to clean up, I can trust that the 
+// lastIsland number is the total islands found
+
 const search = () => {
   const map = [...data]
   let lastIsland = 0
@@ -15,8 +22,14 @@ const search = () => {
   }
   console.log(lastIsland)
   console.log(map.map((row) => console.log(row.join(''))))
+  return lastIsland
 }
 
+// this recursively finds all of the land postions and marks 
+// them with a number.
+// It looks at ever position around the starting position.
+// if it finds an X, it marks it with numb and 
+// calls itself with the next position as the starter
 const checkSurroundings = (y, x, map, num) => {
   for (let i = y - 1; i <= y + 1; i++) {
     for (let j = x - 1; j <= x + 1; j++) {
